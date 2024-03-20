d2b19bdf04368d9c9c1ffa1ebc4ee63d662cf250
asfakur <saikat.hap@gmail.com>
2024-03-20 11:21:12 +0600

    18.5- DaisyUI

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const AddToCart = () => {
   return (
-    <button onClick={() => console.log("Click")}>Click</button>
+    <button className='btn btn-primary' onClick={() => console.log("Click")}>Click</button>
   )
 }
 
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 interface User {
   id: number;
   name: string;
+  email: string;
 }
 const UsersPage = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
@@ -14,11 +15,22 @@ const UsersPage = async () => {
     <>
       <h1>Users</h1>
       <p>{new Date().toLocaleTimeString()}</p>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <table className="table table-zebra">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
