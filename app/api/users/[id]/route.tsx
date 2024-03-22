2ec84b0ec7a930baaa744a6ac60f34bbb11cd676
asfakur <saikat.hap@gmail.com>
2024-03-22 10:13:40 +0600

    34.5- Updating an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -9,3 +9,21 @@ export function GET(
 
   return NextResponse.json({ id: params.id, name: "Rahman" });
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // validate the request body
+  // if invalid, return 400
+  // Fetch the user with the given id form db
+  // If does't exist, return 404
+  // update the user
+  // return the updated user
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  return NextResponse.json({ id: params.id, name: body.name + "x" });
+}
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,8 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function GET(request: NextRequest) {
-    return NextResponse.json([
-        {id: 1, name: "Abdur"},
-        {id: 2, name: "Rahim"}
-    ]);
+  return NextResponse.json([
+    { id: 1, name: "Abdur" },
+    { id: 2, name: "Rahim" },
+  ]);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 }); // if invalid return 400
+  return NextResponse.json({ id: 5, name: body.name }, { status: 201 }); // 201: object is created
 }
