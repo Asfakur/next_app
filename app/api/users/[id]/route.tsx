af3206bdc31e15de3983dbcc227bac235dd438bf
asfakur <saikat.hap@gmail.com>
2024-03-23 12:11:03 +0600

    44.7- Getting Data

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,14 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import prisma from "@/prisma/client";
 
-export function GET(
+export async function GET(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
-  if (params.id > 10)
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
 
-  return NextResponse.json({ id: params.id, name: "Rahman" });
+  return NextResponse.json(user);
 }
 
 export async function PUT(
@@ -25,10 +29,7 @@ export async function PUT(
   const validation = schema.safeParse(body);
 
   if (!validation.success)
-    return NextResponse.json(
-        validation.error.errors,
-      { status: 400 }
-    );
+    return NextResponse.json(validation.error.errors, { status: 400 });
   if (params.id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({ id: params.id, name: body.name + "x" });
